91f141eb961481b9eb123810c8e2ad2aee6c1206
Chris Dugan <c.m.s.dugan@gmail.com>
2019-11-07 19:22:38 -0500

    Add more error messaging

--- src/Randomizer.elm
+++ src/Randomizer.elm
@@ -29,8 +29,12 @@ init _ = (Model None "" Disallowed, Cmd.none )
 
 -- UPDATE
 
-type Msg = RandomNumber Float | Submit RandomResult | CustomTextChange String | CustomButtonSubmit
-type RandomResult = None | Success | Failure
+type Msg = 
+ RandomNumber Float |
+ Submit RandomResult |
+ CustomTextChange String |
+ CustomButtonSubmit
+type RandomResult = None | Success | Failure | Error
 
 generateRandom : Float -> Cmd Msg
 generateRandom f = Random.generate Submit (Random.weighted (f, Success) [ (1.0 - f, Failure) ])
@@ -51,11 +55,9 @@ update msg model =
    , Cmd.none
    )
   CustomButtonSubmit ->
-   ( model
-   , case model.submitButton of
-    Allowed f -> generateRandom f
-    Disallowed -> Cmd.none
-   )
+   case model.submitButton of
+    Allowed f -> ( model, generateRandom f)
+    Disallowed -> ( { model | result = Error }, Cmd.none )
 
 maybeParsePercentage : String -> Maybe Float
 maybeParsePercentage = String.toInt >> Maybe.map (\n -> (toFloat n) / 100.0)
@@ -81,10 +83,11 @@ subscriptions model =
 
 getResultDiv : Model -> Html Msg
 getResultDiv model =
- if model.result == Success then
-  div [ style "color" "green" ] [ text "SUCCESS!" ]
- else
-  div [ style "color" "red" ] [ text "FAILURE!" ]
+ case model.result of
+  Success -> div [ style "color" "green" ] [ text "SUCCESS!" ]
+  Failure -> div [ style "color" "red" ] [ text "FAILURE!" ]
+  None -> div [ style "color" "black" ] [ text "Click a button"]
+  Error -> div [ style "color" "red" ] [ text "ERROR!" ]
 
 toPercentageString : Float -> String
 toPercentageString f =
